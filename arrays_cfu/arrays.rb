c4fb7625464720f2752d7148c28e3d1c26848304
Jesse Sorman <jessesorman@gmail.com>
2023-02-07 19:15:53 -0500

    Add array methods

--- arrays_cfu/arrays.rb
+++ arrays_cfu/arrays.rb
@@ -1,4 +1,19 @@
 grocery_list = ["Eggs", "Milk", "Butter", "Sugar"]
 multiples = [5, 10, 15, 20]
-change = [0.25, 0.05, 0.10, .01]
+change = [0.25, 0.05, 0.10, 0.01]
 booleans = [true, false, true, true]
+
+/push adds an element to the end of an array/
+puts grocery_list.push "Cheese"
+
+/pop removes the last element from an array/
+multiples.pop()
+puts multiples
+
+/shift removes the first element from an array/
+change.shift()
+puts change
+
+/unshift adds an element to the beggining of an array/
+booleans.unshift(false)
+puts booleans
